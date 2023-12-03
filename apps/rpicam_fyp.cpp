a43cf9cb24cadc970c727d96b7d230beee344ab3
edwardatki <edwardatki@gmail.com>
2023-12-03 23:58:45 +0000

    First working demo

--- apps/rpicam_fyp.cpp
+++ apps/rpicam_fyp.cpp
@@ -22,6 +22,9 @@ static void event_loop(RPiCamApp &app)
 	app.ConfigureViewfinder();
 	app.StartCamera();
 
+	unsigned int luminance_size = 0;
+	uint8_t* previous_frame = NULL;
+
 	auto start_time = std::chrono::high_resolution_clock::now();
 
 	for (unsigned int count = 0; ; count++)
@@ -39,12 +42,46 @@ static void event_loop(RPiCamApp &app)
 		else if (msg.type != RPiCamApp::MsgType::RequestComplete)
 			throw std::runtime_error("unrecognised message!");
 
-		LOG(2, "Viewfinder frame " << count);
+		// LOG(2, "Viewfinder frame " << count);
 		auto now = std::chrono::high_resolution_clock::now();
 		if (options->timeout && (now - start_time) > options->timeout.value)
 			return;
 
 		CompletedRequestPtr &completed_request = std::get<CompletedRequestPtr>(msg.payload);
+
+		// Get frame
+		BufferWriteSync w(&app, completed_request->buffers[app.GetMainStream()]);
+		libcamera::Span<uint8_t> buffer = w.Get()[0];
+		uint8_t *image = (uint8_t *)buffer.data();
+
+		if (count == 0) {
+			libcamera::StreamConfiguration const &cfg = app.GetMainStream()->configuration();
+			LOG(1, "Raw stream: " << cfg.size.width << "x" << cfg.size.height << " stride " << cfg.stride << " format "
+								  << cfg.pixelFormat.toString());
+			luminance_size = cfg.size.width * cfg.size.height;
+			previous_frame = (uint8_t*)malloc(luminance_size);
+		}
+
+		// Process luminance
+		for (unsigned int i = 0; i < luminance_size ; i += 1) {
+			// How to extract luminance
+			uint8_t* new_value = image+i;
+			uint8_t* old_value = previous_frame+i;
+			int16_t diff = *new_value-*old_value;
+			*old_value = *new_value;
+
+			int16_t threshold = 40;
+			if (std::abs(diff) > threshold) *new_value = (diff > 0) ? 255 : 0;
+			else *new_value = 127;
+		}
+
+		// To black and white
+		for (unsigned int i = luminance_size; i < buffer.size() ; i += 1) {
+			*(image+i) = 127;
+		}
+
+
+
 		app.ShowPreview(completed_request, app.ViewfinderStream());
 	}
 }
